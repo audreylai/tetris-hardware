96681c5ee834a9cd7fd06e6eb7b3a3c3f4c7ef4f
Audrey Lai <68845532+audreylai@users.noreply.github.com>
2023-05-25 15:41:00 +0800

    Fixed draw function

--- hardware.py
+++ hardware.py
@@ -1,3 +1,4 @@
+from machine import Pin
 import time
 from neopixel import Neopixel
 
@@ -5,21 +6,39 @@ LEDS = 30 # How many LEDs?
 PIN = 28
 pixels = Neopixel(LEDS, 0, PIN, "GRB")
 
+
+buttons = [Pin(x, Pin.IN, Pin.PULL_DOWN) for x in [21, 20, 17, 18]]
+
+
 def draw(xin, yin, color):
     def pattern(r, c):
-        if r <= 7:
-            if c % 2 == 0:
-                result = 7 * c + c + r
-            elif c % 2 == 1:
-                result = 7 * (c+1) - r + c
-        if r > 7: #top half of the board
-            result = grid[r-8][c] + 128
+        if r <= 15:
+            if c % 2 == 0: # humanly odd, computationall even
+                result = (16 * c) + r
+            elif c % 2 == 1: # human even, computer odd
+                result = 16 * (c+1) - r - 1
+        if r > 15: #top half of the board
+            result = grid[r-16][c] + 512
         return result
+            
+            
 
-    grid = [[ 0 for x in range(16)] for y in range(16)]
-    for y in range(len(grid)):
-        for x in range(len(grid[y])):
+    grid = [[ 0 for x in range(32)] for y in range(32)]
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
             grid[y][x] = pattern(y,x)
     coord = grid[yin][xin]
 
-    pixels
+    pixels[coord] = color
+
+def get_button_press(key):
+    key = {
+        "rotate": 0,
+        "drop": 1,
+        "hold": 2,
+        "start": 3
+    }
+    
+    return buttons[key].value()
+
+
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import machine, time
-led = machine.Pin("LED", machine.Pin.OUT)
-for i in range(0, 10):
-    print(i)
-    led.on()
-    time.sleep(0.5)
-    led.off()
-    time.sleep(0.5)
+from hardware import *
+from machine import Pin
+import time
+
+# while True:
+#     if get_button_press("rotate"):
+#         print("Button is being pressed")
+#     else:
+#         print("Button is not pressed")
+#     time.sleep(0.1)
+
+for x in range(32):
+    for y in range(32):
+        draw(y,x,(255,255,255))
