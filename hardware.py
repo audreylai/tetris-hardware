d03b50e1c4e2efb79c380b6428b8ad9dfb1e7729
Audrey Lai <68845532+audreylai@users.noreply.github.com>
2023-06-05 12:21:31 +0800

    Initialize game and draw piece functions

--- hardware.py
+++ hardware.py
@@ -2,43 +2,63 @@ from machine import Pin
 import time
 from neopixel import Neopixel
 
-LEDS = 30 # How many LEDs?
+LEDS = 1024 # How many LEDs?
 PIN = 28
 pixels = Neopixel(LEDS, 0, PIN, "GRB")
 
 
 buttons = [Pin(x, Pin.IN, Pin.PULL_DOWN) for x in [21, 20, 17, 18]]
+joystick = [Pin(x, Pin.IN, Pin.PULL_DOWN) for x in [12, 11, 10]]
 
-
-def draw(xin, yin, color):
-    def pattern(r, c):
+def pattern(r, c):
         if r <= 15:
             if c % 2 == 0: # humanly odd, computationall even
                 result = (16 * c) + r
             elif c % 2 == 1: # human even, computer odd
-                result = 16 * (c+1) - r - 1
+                result = 16 * c + 15 - r 
         if r > 15: #top half of the board
             result = grid[r-16][c] + 512
+        #print(r, c, result, "hi")
         return result
-            
-            
 
-    grid = [[ 0 for x in range(32)] for y in range(32)]
-    for x in range(len(grid)):
-        for y in range(len(grid[x])):
-            grid[y][x] = pattern(y,x)
-    coord = grid[yin][xin]
+grid = [[ 0 for x in range(32)] for y in range(32)]
+for y in range(len(grid)):
+    for x in range(len(grid[y])):
+        grid[y][x] = pattern(y,x)
+
+
+def draw(xin, yin, color):
+    coord = pattern(yin,xin)
 
     pixels[coord] = color
 
 def get_button_press(key):
-    key = {
+    ref = {
         "rotate": 0,
         "drop": 1,
         "hold": 2,
         "start": 3
     }
-    
-    return buttons[key].value()
+    return buttons[ref[key]].value()
 
+def joystick_moved(key):
+    ref = {
+        "left":0,
+        "right":1,
+        "down":2
+    }
+    return joystick[ref[key]].value()
+
+def draw_rect(coord1, coord2, color, border=True):
+    x1, y1 = coord1
+    x2, y2 = coord2
+    for x in range(x1, x2+1):
+        for y in range(y1, y2+1):
+            draw(x, y, color)
+    
+    if border:
+        draw_rect((x1+1, y1+1), (x2-1, y2-1), (0,0,0), False)
+    pixels.show()
+    
+    
 
--- main.py
+++ main.py
@@ -2,13 +2,94 @@ from hardware import *
 from machine import Pin
 import time
 
-# while True:
-#     if get_button_press("rotate"):
-#         print("Button is being pressed")
-#     else:
-#         print("Button is not pressed")
-#     time.sleep(0.1)
+def init_game():
+    border_color = (69, 71, 105)
+    border_color = (10, 10 ,10)
+    draw_rect((1, 3), (8, 8), border_color) # hold box
+    
+    draw_rect((1, 10), (8, 20), border_color) # digits box
+    
+    draw_rect((10, 3), (21, 28), border_color) # game space
+    
+    draw_rect((23, 3), (30, 28), border_color)
 
+
+def draw_pieces(coord, key, rotation):
+    pieces = {
+        "0": [
+                [1, 1, 1, 1]
+            ],
+        "1": [
+                [1, 0, 0],
+                [1, 1, 1]
+            ],
+        "2": [
+                [0, 0, 1],
+                [1, 1, 1]
+            ],
+        "3": [
+                [1, 1, 0],
+                [1, 1, 0]
+            ],
+        "4": [
+                [0, 1, 1],
+                [1, 1, 0]
+            ],
+        "5": [
+                [0, 1, 0],
+                [1, 1, 1]
+            ],
+        "6": [
+                [1, 1, 0],
+                [0, 1, 1]
+            ],
+    }
+    piece = pieces[key]
+    for i in range(rotation):
+        # piece = list(zip(*piece))
+        # piece = [list(reversed(x)) for x in piece]
+        piece = list(list(x) for x in zip(*piece))[::-1]
+    
+
+    for x in range(len(piece[0])):
+        for y in range(len(piece)):
+            if piece[y][x] == 1:
+                draw(coord[0]+x, coord[1]+y, (10, 10, 100))
+    pixels.show()
+
+
+
+
+init_game()
+for i in range(4):
+    draw_pieces((11,4), "0", i)
+    time.sleep(1)
+    draw_rect((11,4), (15,8), (0,0,0), False)
+
+
+x = 0
+y = 0
+
+'''
+while True:
+    if joystick_moved("left"):
+        x-=1
+    if joystick_moved("right"):
+        x+=1
+    if joystick_moved("down"):
+        y += 1
+    draw(x, y, (20, 0, 64))
+    pixels.show()
+    
+'''
+"""
+for i in range(1):
+    pixels[i] = [0, 0, 64]
+    pixels.show()
+print(pixels)
 for x in range(32):
     for y in range(32):
-        draw(y,x,(255,255,255))
+        draw(y,x,(30,10,0))
+"""
+
+
