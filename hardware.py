582a2d15e8e08499bb1961d2c7082faae8f1401c
Audrey Lai <68845532+audreylai@users.noreply.github.com>
2023-06-27 12:13:03 +0800

    yassified tetris

--- hardware.py
+++ hardware.py
@@ -1,74 +1,82 @@
-from machine import Pin
 import time
-from neopixel import Neopixel
+import board
+import neopixel
 import random
+import RPi.GPIO as GPIO
 
 LEDS = 1024 # How many LEDs?
-PIN = 21
-pixels = Neopixel(LEDS, 0, PIN, "GRB")
+pixels = neopixel.NeoPixel(board.D21, LEDS, auto_write=False)
+
+class Pin:
+	def __init__(self, pin_n):
+		self.pin_n = pin_n
+		GPIO.setup(pin_n, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+	def value(self):
+		return GPIO.input(self.pin_n) == GPIO.HIGH
+
+buttons = [Pin(x) for x in [26, 20, 17, 18]]
+joystick = [Pin(x) for x in [12, 11, 10]]
 
 def pattern(r, c):
-        if r <= 15:
-            if c % 2 == 0: # humanly odd, computationall even
-                result = (16 * c) + r
-            elif c % 2 == 1: # human even, computer odd
-                result = 16 * c + 15 - r 
-        if r > 15: #top half of the board
-            result = grid[r-16][c] + 512
-        #print(r, c, result, "hi")
-        return result
-
-def draw(xin, yin, color):
-    coord = pattern(yin,xin)
-    pixels[coord] = color
-
-buttons = [Pin(x, Pin.IN, Pin.PULL_DOWN) for x in [21, 20, 17, 18]]
-joystick = [Pin(x, Pin.IN, Pin.PULL_DOWN) for x in [12, 11, 10]]
+		if r <= 15:
+			if c % 2 == 0: # humanly odd, computationall even
+				result = (16 * c) + r
+			elif c % 2 == 1: # human even, computer odd
+				result = 16 * c + 15 - r 
+		if r > 15: #top half of the board
+			result = grid[r-16][c] + 512
+		#print(r, c, result, "hi")
+		return result
 
 grid = [[ 0 for x in range(32)] for y in range(32)]
 for y in range(len(grid)):
-    for x in range(len(grid[y])):
-        grid[y][x] = pattern(y,x)
+	for x in range(len(grid[y])):
+		grid[y][x] = pattern(y,x)
 
 def get_pixel_val(x, y):
-    coord = pattern(y,x)
-    return pixels[coord]
+	coord = pattern(y,x)
+	return pixels[coord]
+
+def draw(xin, yin, color):
+	coord = pattern(yin,xin)
+	pixels[coord] = color
 
 def draw_grid(grid):
-    for y in range(len(grid)):
-        for x in range(len(grid[0])):
-            draw(x+11, y+4, grid[y][x])
+	for y in range(len(grid)):
+		for x in range(len(grid[0])):
+			draw(x+11, y+4, grid[y][x])
 
 def get_button_press(key):
-    ref = {
-        "rotate": 0,
-        "drop": 1,
-        "hold": 2,
-        "start": 3
-    }
-    return buttons[ref[key]].value()
+	ref = {
+		"rotate": 0,
+		"drop": 1,
+		"hold": 2,
+		"start": 3
+	}
+	return buttons[ref[key]].value()
 
 def joystick_moved(key):
-    ref = {
-        "left":0,
-        "right":1,
-        "down":2
-    }
-    return joystick[ref[key]].value()
+	ref = {
+		"left":0,
+		"right":1,
+		"down":2
+	}
+	return joystick[ref[key]].value()
 
 def draw_rect(coord1, coord2, color, border=True):
-    x1, y1 = coord1
-    x2, y2 = coord2
-    for x in range(x1, x2+1):
-        for y in range(y1, y2+1):
-            draw(x, y, color)
-    
-    if border:
-        draw_rect((x1+1, y1+1), (x2-1, y2-1), (0,0,0), False)
+	x1, y1 = coord1
+	x2, y2 = coord2
+	for x in range(x1, x2+1):
+		for y in range(y1, y2+1):
+			draw(x, y, color)
+	
+	if border:
+		draw_rect((x1+1, y1+1), (x2-1, y2-1), (0,0,0), False)
 
 def random_shuffle(seq):
-    l = len(seq)
-    for i in range(l):
-        j = random.randrange(l)
-        seq[i], seq[j] = seq[j], seq[i]
-    return seq
+	l = len(seq)
+	for i in range(l):
+		j = random.randrange(l)
+		seq[i], seq[j] = seq[j], seq[i]
+	return seq
--- main.py
+++ main.py
@@ -16,368 +16,380 @@ score = 0
 border_color = (10, 10 ,10)
 
 def init_game():
-    global next_pieces, cursor_coord, rotation, floor_pieces, grid, hold_piece_key
-    # draw borders
-    draw_rect((1, 3), (8, 8), border_color) # hold box
-    
-    draw_rect((1, 10), (8, 20), border_color) # digits box
-    
-    draw_rect((10, 3), (21, 28), border_color) # game space
-    
-    draw_rect((23, 3), (30, 28), border_color) # next
-    
-    
-    cursor_coord = [5, 1]
-    rotation = 0
-    next_pieces = [str(random.randint(0,6)) for x in range(6)]
-    floor_pieces = []
-    grid = [[(0, 0, 0) for i in range(10)] for x in range(24)]
-    hold_piece_key = ""
-    
-    for next_p, y_coord in zip(next_pieces, range(6,27,4)):
-        draw_piece((26, y_coord), next_p, 0, is_grid=False)
+	global next_pieces, cursor_coord, rotation, floor_pieces, grid, hold_piece_key, level, score
+	# draw borders
+	draw_rect((1, 3), (8, 8), border_color) # hold box
+	
+	draw_rect((1, 10), (8, 20), border_color) # digits box
+	
+	draw_rect((10, 3), (21, 28), border_color) # game space
+	
+	draw_rect((23, 3), (30, 28), border_color) # next
+	
+	
+	cursor_coord = [5, 1]
+	rotation = 0
+	next_pieces = [str(random.randint(0,6)) for x in range(6)]
+	floor_pieces = []
+	grid = [[(0, 0, 0) for i in range(10)] for x in range(24)]
+	hold_piece_key = ""
+	level = 0
+	score = 0
+	
+	for next_p, y_coord in zip(next_pieces, range(6,27,4)):
+		draw_piece((26, y_coord), next_p, 0, is_grid=False)
 
 # piece
 def get_piece(key, rotation):
-    pieces = {
-        "0": [
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 1, 1, 1, 1],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0]
-            ],
-        "1": [
-                [1, 0, 0],
-                [1, 1, 1],
-                [0, 0, 0]
-            ],
-        "2": [
-                [0, 0, 1],
-                [1, 1, 1],
-                [0, 0, 0]
-            ],
-        "3": [
-                [1, 1],
-                [1, 1]
-            ],
-        "4": [
-                [0, 1, 1],
-                [1, 1, 0],
-                [0, 0, 0]
-            ],
-        "5": [
-                [0, 1, 0],
-                [1, 1, 1],
-                [0, 0, 0]
-            ],
-        "6": [
-                [1, 1, 0],
-                [0, 1, 1],
-                [0, 0, 0]
-            ],
-    }
-    piece = pieces[key]
-    for i in range(rotation):
-        piece = list(zip(*piece))
-        piece = [list(reversed(x)) for x in piece]
-    return piece
+	pieces = {
+		"0": [
+				[0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0],
+				[0, 1, 1, 1, 1],
+				[0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0]
+			],
+		"1": [
+				[1, 0, 0],
+				[1, 1, 1],
+				[0, 0, 0]
+			],
+		"2": [
+				[0, 0, 1],
+				[1, 1, 1],
+				[0, 0, 0]
+			],
+		"3": [
+				[1, 1],
+				[1, 1]
+			],
+		"4": [
+				[0, 1, 1],
+				[1, 1, 0],
+				[0, 0, 0]
+			],
+		"5": [
+				[0, 1, 0],
+				[1, 1, 1],
+				[0, 0, 0]
+			],
+		"6": [
+				[1, 1, 0],
+				[0, 1, 1],
+				[0, 0, 0]
+			],
+	}
+	piece = pieces[key]
+	for i in range(rotation):
+		piece = list(zip(*piece))
+		piece = [list(reversed(x)) for x in piece]
+	return piece
 
 def get_piece_coord(coord, piece):
-    center_y, center_x = math.floor(len(piece)/2), math.floor(len(piece[0])/2)
-    piece_coords = []
-    for y in range(len(piece)):
-        for x in range(len(piece[0])):
-            if piece[y][x] == 1:
-                piece_coords.append([x + coord[0] - center_x, y + coord[1] - center_y])
-    return piece_coords
+	center_y, center_x = math.floor(len(piece)/2), math.floor(len(piece[0])/2)
+	piece_coords = []
+	for y in range(len(piece)):
+		for x in range(len(piece[0])):
+			if piece[y][x] == 1:
+				piece_coords.append([x + coord[0] - center_x, y + coord[1] - center_y])
+	return piece_coords
 
 def draw_piece(coord, key, rotation, undraw=False, is_grid=True):
-    global grid
-    piece_colors = {
-        "0": [x*0.1 for x in (103, 225, 230)],
-        "1": [x*0.1 for x in (56, 72, 214)],
-        "2": [x*0.1 for x in (222, 163, 44)],
-        "3": [x*0.1 for x in (247, 247, 87)],
-        "4": [x*0.1 for x in (88, 224, 67)],
-        "5": [x*0.1 for x in (161, 38, 209)],
-        "6": [x*0.1 for x in (217, 30, 55)]
-    }
-    piece = get_piece(key, rotation)
-    piece_coords = get_piece_coord(coord, piece)
-    
-    if not undraw:
-        for x, y in piece_coords:
-            color = piece_colors[key]
-            if not is_grid:
-                draw(x, y, color)
-            else:
-                grid[y][x] = color
-    else:
-        for x, y in piece_coords:
-            color = piece_colors[key]
-            if not is_grid:
-                draw(x, y, (0,0,0))
-            else:
-                grid[y][x] = (0, 0, 0)
+	global grid
+	piece_colors = {
+		"0": [x*0.1 for x in (103, 225, 230)],
+		"1": [x*0.1 for x in (56, 72, 214)],
+		"2": [x*0.1 for x in (222, 163, 44)],
+		"3": [x*0.1 for x in (247, 247, 87)],
+		"4": [x*0.1 for x in (88, 224, 67)],
+		"5": [x*0.1 for x in (161, 38, 209)],
+		"6": [x*0.1 for x in (217, 30, 55)]
+	}
+	piece = get_piece(key, rotation)
+	piece_coords = get_piece_coord(coord, piece)
+	
+	if not undraw:
+		for x, y in piece_coords:
+			color = piece_colors[key]
+			if not is_grid:
+				draw(x, y, color)
+			else:
+				grid[y][x] = color
+	else:
+		for x, y in piece_coords:
+			color = piece_colors[key]
+			if not is_grid:
+				draw(x, y, (0,0,0))
+			else:
+				grid[y][x] = (0, 0, 0)
 
 
 # utility
 def is_collide(new_cursor_coord, piece_key, rotation, floor_only=False):
-    piece = get_piece(piece_key, rotation)
-    piece_coords = get_piece_coord(new_cursor_coord, piece)
-    
-    if not floor_only:   
-        # check wall bounds
-        for pc_x, pc_y in piece_coords:
-            if pc_x < 0 or pc_x > 9 or pc_y < 0 or pc_y > 23:
-                return True
-    
-    for pc in piece_coords:
-        if pc[1] > 23 or pc in floor_pieces:
-            return True
-    
+	piece = get_piece(piece_key, rotation)
+	piece_coords = get_piece_coord(new_cursor_coord, piece)
+	
+	if not floor_only:   
+		# check wall bounds
+		for pc_x, pc_y in piece_coords:
+			if pc_x < 0 or pc_x > 9 or pc_y < 0 or pc_y > 23:
+				return True
+	
+	for pc in piece_coords:
+		if pc[1] > 23 or pc in floor_pieces:
+			return True
+	
 
-    return False
+	return False
 
 def drop_piece(piece_key, rotation):
-    global floor_pieces, cursor_coord
-    while not is_collide((cursor_coord[0], cursor_coord[1]+1), piece_key, rotation, floor_only=True):
-        cursor_coord[1] += 1
+	global floor_pieces, cursor_coord
+	while not is_collide((cursor_coord[0], cursor_coord[1]+1), piece_key, rotation, floor_only=True):
+		cursor_coord[1] += 1
 
 
 # managment
 def manage_next_pieces():
-    global next_pieces
-    tmp_next = []
-    
-    draw_rect((24, 4), (29, 27), (0,0,0), border=False)
+	global next_pieces
+	tmp_next = []
+	
+	draw_rect((24, 4), (29, 27), (0,0,0), border=False)
 
-    if len(tmp_next) == 0:
-        tmp_next = random_shuffle([str(x) for x in range(0, 7)])
-    
-    next_pieces.append(tmp_next.pop(0))
-    for next_p, y_coord in zip(next_pieces, range(6,27,4)):
-        draw_piece((26, y_coord), next_p, 0, undraw=False, is_grid=False)
-    draw_rect((23, 28), (30, 28), border_color)
+	if len(tmp_next) == 0:
+		tmp_next = random_shuffle([str(x) for x in range(0, 7)])
+	
+	next_pieces.append(tmp_next.pop(0))
+	for next_p, y_coord in zip(next_pieces, range(6,27,4)):
+		draw_piece((26, y_coord), next_p, 0, undraw=False, is_grid=False)
+	draw_rect((23, 28), (30, 28), border_color)
 
 def swap_hold_piece(new_piece):
-    global hold_piece_key, next_pieces
+	global hold_piece_key, next_pieces
 
-    # swap
-    if hold_piece_key == "":
-        old_hold_piece_key = next_pieces.pop(0)
-    else:
-        old_hold_piece_key = hold_piece_key
-    
-    hold_piece_key = new_piece
+	# swap
+	if hold_piece_key == "":
+		old_hold_piece_key = next_pieces.pop(0)
+	else:
+		old_hold_piece_key = hold_piece_key
+	
+	hold_piece_key = new_piece
 
-    # redraw hold
-    draw_rect((1, 3), (8, 8), border_color)
-    draw_piece((4, 6), hold_piece_key, 0, is_grid=False)
+	# redraw hold
+	draw_rect((1, 3), (8, 8), border_color)
+	draw_piece((4, 6), hold_piece_key, 0, is_grid=False)
 
-    return old_hold_piece_key
+	return old_hold_piece_key
 
 def manage_level():
-    global level
-    numbers = {
-        "0": [
-            [0, 1, 1, 0],
-            [1, 0, 0, 1],
-            [1, 0, 0, 1],
-            [1, 0, 0, 1],
-            [1, 0, 0, 1],
-            [1, 0, 0, 1],
-            [0, 1, 1, 0]
-        ],
-        "1": [
-            [0, 0, 1, 0],
-            [0, 1, 1, 0],
-            [1, 0, 1, 0],
-            [0, 0, 1, 0],
-            [0, 0, 1, 0],
-            [0, 0, 1, 0],
-            [0, 0, 1, 0]
-        ],
-        "2": [
-            [0, 1, 1, 0],
-            [1, 0, 0, 1],
-            [0, 0, 0, 1],
-            [0, 1, 1, 0],
-            [1, 0, 0, 0],
-            [1, 0, 0, 0],
-            [1, 1, 1, 1],
-        ],
-        "3": [
-            [0, 1, 1, 0],
-            [1, 0, 0, 1],
-            [0, 0, 0, 1],
-            [0, 1, 1, 0],
-            [0, 0, 0, 1],
-            [1, 0, 0, 1],
-            [0, 1, 1, 0],
-        ],
-        "4": [
-            [1, 0, 0, 1],
-            [1, 0, 0, 1],
-            [1, 0, 0, 1],
-            [0, 1, 1, 1],
-            [0, 0, 0, 1],
-            [0, 0, 0, 1],
-            [0, 0, 0, 1],
-        ],
-        "5": [
-            [1, 1, 1, 1],
-            [1, 0, 0, 0],
-            [1, 1, 1, 0],
-            [0, 0, 0, 1],
-            [0, 0, 0, 1],
-            [0, 0, 0, 1],
-            [1, 1, 1, 0],
-        ],
-        "6": [
-            [0, 1, 1, 0],
-            [1, 0, 0, 1],
-            [1, 0, 0, 0],
-            [1, 1, 1, 0],
-            [1, 0, 0, 1],
-            [1, 0, 0, 1],
-            [0, 1, 1, 0],
-        ],
-        "7": [
-            [1, 1, 1, 1],
-            [0, 0, 0, 1],
-            [0, 0, 0, 1],
-            [0, 0, 1, 0],
-            [0, 0, 1, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-        ],
-        "8": [
-            [0, 1, 1, 0],
-            [1, 0, 0, 1],
-            [1, 0, 0, 1],
-            [0, 1, 1, 0],
-            [1, 0, 0, 1],
-            [1, 0, 0, 1],
-            [0, 1, 1, 0],
-        ],
-        "9": [
-            [0, 1, 1, 0],
-            [1, 0, 0, 1],
-            [1, 0, 0, 1],
-            [0, 1, 1, 1],
-            [0, 0, 0, 1],
-            [1, 0, 0, 1],
-            [0, 1, 1, 0],
-        ]
-    }
-    number = numbers[str(level)]
-    for x, y in zip(range(0, len(number[0])), range(len(number))):
-        number_color = (183, 123, 209) if number[y][x] == 1 else (0, 0, 0)
-        draw(x, y, color=number_color)
+	global level, score
+
+	if score > level**2 * 1000:
+		level += 1
+
+	numbers = {
+		"0": [
+			[0, 1, 1, 0],
+			[1, 0, 0, 1],
+			[1, 0, 0, 1],
+			[1, 0, 0, 1],
+			[1, 0, 0, 1],
+			[1, 0, 0, 1],
+			[0, 1, 1, 0]
+		],
+		"1": [
+			[0, 0, 1, 0],
+			[0, 1, 1, 0],
+			[1, 0, 1, 0],
+			[0, 0, 1, 0],
+			[0, 0, 1, 0],
+			[0, 0, 1, 0],
+			[0, 0, 1, 0]
+		],
+		"2": [
+			[0, 1, 1, 0],
+			[1, 0, 0, 1],
+			[0, 0, 0, 1],
+			[0, 1, 1, 0],
+			[1, 0, 0, 0],
+			[1, 0, 0, 0],
+			[1, 1, 1, 1],
+		],
+		"3": [
+			[0, 1, 1, 0],
+			[1, 0, 0, 1],
+			[0, 0, 0, 1],
+			[0, 1, 1, 0],
+			[0, 0, 0, 1],
+			[1, 0, 0, 1],
+			[0, 1, 1, 0],
+		],
+		"4": [
+			[1, 0, 0, 1],
+			[1, 0, 0, 1],
+			[1, 0, 0, 1],
+			[0, 1, 1, 1],
+			[0, 0, 0, 1],
+			[0, 0, 0, 1],
+			[0, 0, 0, 1],
+		],
+		"5": [
+			[1, 1, 1, 1],
+			[1, 0, 0, 0],
+			[1, 1, 1, 0],
+			[0, 0, 0, 1],
+			[0, 0, 0, 1],
+			[0, 0, 0, 1],
+			[1, 1, 1, 0],
+		],
+		"6": [
+			[0, 1, 1, 0],
+			[1, 0, 0, 1],
+			[1, 0, 0, 0],
+			[1, 1, 1, 0],
+			[1, 0, 0, 1],
+			[1, 0, 0, 1],
+			[0, 1, 1, 0],
+		],
+		"7": [
+			[1, 1, 1, 1],
+			[0, 0, 0, 1],
+			[0, 0, 0, 1],
+			[0, 0, 1, 0],
+			[0, 0, 1, 0],
+			[0, 1, 0, 0],
+			[0, 1, 0, 0],
+		],
+		"8": [
+			[0, 1, 1, 0],
+			[1, 0, 0, 1],
+			[1, 0, 0, 1],
+			[0, 1, 1, 0],
+			[1, 0, 0, 1],
+			[1, 0, 0, 1],
+			[0, 1, 1, 0],
+		],
+		"9": [
+			[0, 1, 1, 0],
+			[1, 0, 0, 1],
+			[1, 0, 0, 1],
+			[0, 1, 1, 1],
+			[0, 0, 0, 1],
+			[1, 0, 0, 1],
+			[0, 1, 1, 0],
+		]
+	}
+	number = numbers[str(level)]
+	for x in range(0, len(number[0])):
+		for y in range(len(number)):
+			number_color = (36, 70, 87) if number[y][x] == 1 else (0,0,0)
+			draw(x+3, y+12, color=number_color)
 
 # check functions
 def check_is_full():
-    global floor_pieces
-    return any([True for fp in floor_pieces if fp[1] < 1])
+	global floor_pieces
+	return any([True for fp in floor_pieces if fp[1] < 1])
 
 def check_clear_lines():
-    global floor_pieces, score, level
-    n_clears = 0
-    for y in range(24):
-        if len([fp for fp in floor_pieces if fp[1] == y]) == 10:
-            n_clears += 1
-            floor_pieces = [fp for fp in floor_pieces if fp[1] != y]
-            for i in range(len(floor_pieces)):
-                if floor_pieces[i][1] < y:
-                    floor_pieces[i][1] += 1
-            grid.pop(y)
-            grid.insert(0, [(0,0,0) for abc in range(10)])
-    if n_clears == 4:
-        score += 800 * level
-    elif n_clears > 0:
-        score += n_clears*100 + (n_clears-1)*100
+	global floor_pieces, score, level
+	n_clears = 0
+	for y in range(24):
+		if len([fp for fp in floor_pieces if fp[1] == y]) == 10:
+			n_clears += 1
+			floor_pieces = [fp for fp in floor_pieces if fp[1] != y]
+			for i in range(len(floor_pieces)):
+				if floor_pieces[i][1] < y:
+					floor_pieces[i][1] += 1
+			grid.pop(y)
+			grid.insert(0, [(0,0,0) for abc in range(10)])
+	if n_clears == 4:
+		score += 800 * level
+	elif n_clears > 0:
+		score += n_clears*100 + (n_clears-1)*100
 
 def game_loop():
-    global rotation, cursor_coord, next_pieces, floor_pieces, game_speed
-    can_move = False
-    already_hold = False
+	global rotation, cursor_coord, next_pieces, floor_pieces, game_speed, score
+	can_move = False
+	already_hold = False
 
-    # timers
-    rotation_timer = time.time()
-    drop_timer = time.time()
-    move_timer = time.time()
-    joystick_timer = time.time()
-    check_timer = time.time()
+	# timers
+	rotation_timer = time.time()
+	drop_timer = time.time()
+	move_timer = time.time()
+	joystick_timer = time.time()
+	check_timer = time.time()
 
-    piece_key = None
-    while True:
-        if get_button_press("start") or check_is_full():
-            init_game()
-            can_move = False
-        if not can_move:
-            piece_key = next_pieces.pop(0)
-            rotation = 0
-            cursor_coord = [5, 1]
-            manage_next_pieces()
-            can_move = True
+	piece_key = None
+	old_game_speed = game_speed
+	while True:
+		if get_button_press("start") or check_is_full():
+			# plau boing
+			init_game()
+			can_move = False
+		if not can_move:
+			piece_key = next_pieces.pop(0)
+			rotation = 0
+			cursor_coord = [5, 1]
+			manage_next_pieces()
+			can_move = True
 
-        check_clear_lines()
-        draw_piece(cursor_coord, piece_key, rotation, undraw=True)
-        
-        # joystick controls
-        if time.time() - joystick_timer > 0.09:
-            if joystick_moved("left") and not is_collide((cursor_coord[0]-1, cursor_coord[1]), piece_key, rotation):
-                cursor_coord[0]-=1
-                joystick_timer = time.time()
-            if joystick_moved("right") and not is_collide((cursor_coord[0]+1, cursor_coord[1]), piece_key, rotation):
-                cursor_coord[0]+=1
-                joystick_timer = time.time()
-            if joystick_moved("down") and not is_collide((cursor_coord[0], cursor_coord[1]+1), piece_key, rotation):
-                game_speed /= 2
-                joystick_timer = time.time()
-            else:
-                game_speed = 0.2
-        
-        if get_button_press("hold") and not already_hold:
-            piece_key = swap_hold_piece(piece_key)
-            rotation = 0
-            cursor_coord = [5, 1]
-            manage_next_pieces()
-            already_hold = True
-        if get_button_press("drop") and (time.time() - drop_timer > 1):
-            drop_piece(piece_key, rotation)
-            drop_timer = time.time()
-            check_timer = 1
-        if get_button_press("rotate") and (time.time() - rotation_timer > 0.2):
-            if not is_collide(cursor_coord, piece_key, rotation + 1):
-                rotation += 1
-            else:
-                pass
-            rotation_timer = time.time()
-        
-        # moving down / gravity
-        if not is_collide((cursor_coord[0], cursor_coord[1]+1), piece_key, rotation, floor_only=True):
-            if (time.time() - move_timer > game_speed):
-                cursor_coord[1] += 1
-                move_timer = time.time()
-            check_timer = time.time()
-        elif (time.time() - check_timer > 0.5):
-            for c in get_piece_coord((cursor_coord[0], cursor_coord[1]), get_piece(piece_key, rotation)):
-                floor_pieces.append(c)
-            can_move = False
-            already_hold = False
-        
-        draw_piece(cursor_coord, piece_key, rotation)
+		check_clear_lines()
+		draw_piece(cursor_coord, piece_key, rotation, undraw=True)
+		manage_level()
+		# joystick controls
+		if time.time() - joystick_timer > 0.09:
+			if joystick_moved("left") and not is_collide((cursor_coord[0]-1, cursor_coord[1]), piece_key, rotation):
+				cursor_coord[0]-=1
+				joystick_timer = time.time()
+			if joystick_moved("right") and not is_collide((cursor_coord[0]+1, cursor_coord[1]), piece_key, rotation):
+				cursor_coord[0]+=1
+				joystick_timer = time.time()
+			if joystick_moved("down") and not is_collide((cursor_coord[0], cursor_coord[1]+1), piece_key, rotation):
+				game_speed = 0.01
+				joystick_timer = time.time()
+			else:
+				game_speed = 0.2
+		
+		if get_button_press("rotate") and (time.time() - rotation_timer > 0.2):
+			print("HELLOOOOO")
+			if not is_collide(cursor_coord, piece_key, rotation + 1):
+				print("okay")
+				rotation += 1
+			else:
+				pass
+			rotation_timer = time.time()
 
-        if rotation > 3:
-            rotation = 0
+		if get_button_press("hold") and not already_hold:
+			piece_key = swap_hold_piece(piece_key)
+			rotation = 0
+			cursor_coord = [5, 1]
+			manage_next_pieces()
+			already_hold = True
+		if get_button_press("drop") and (time.time() - drop_timer > 0.2):
+			drop_piece(piece_key, rotation)
+			drop_timer = time.time()
+			check_timer = 1
+		
+		# moving down / gravity
+		if not is_collide((cursor_coord[0], cursor_coord[1]+1), piece_key, rotation, floor_only=True):
+			if (time.time() - move_timer > game_speed):
+				cursor_coord[1] += 1
+				move_timer = time.time()
+			check_timer = time.time()
+		elif (time.time() - check_timer > 0.5):
+			for c in get_piece_coord((cursor_coord[0], cursor_coord[1]), get_piece(piece_key, rotation)):
+				floor_pieces.append(c)
+			can_move = False
+			already_hold = False
+		
+		draw_piece(cursor_coord, piece_key, rotation)
 
-        draw_grid(grid)
-        pixels.show()
+		if rotation > 3:
+			rotation = 0
+
+		draw_grid(grid)
+		pixels.show()
 
 if __name__ == "__main__":
-    init_game()
-    pixels.show()
-    game_loop()
+	init_game()
+	pixels.show()
+	game_loop()
